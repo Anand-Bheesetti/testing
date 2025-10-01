30684b340fd72e44ef930bde01e9057773273a92
Anand-Bheesetti <anand.bheesetti@techolution.com>
2025-10-01 19:52:55 +0530

    Create iam_service_principal.tf

---
+++ iam_service_principal.tf
@@ -0,0 +1,27 @@
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "my-lambda-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_dynamo_policy_violation" {
+  name = "lambda-dynamo-policy-violation"
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = "dynamodb:GetItem",
+      // VIOLATION: Wildcard resource grants access to all tables.
+      Resource = "arn:aws:dynamodb:us-east-1:123456789012:table/*" 
+    }]
+  })
+}
