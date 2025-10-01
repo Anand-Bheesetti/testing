6fac00e87dbc6474fd888d7846fbf22778906a4e
Anand-Bheesetti <anand.bheesetti@techolution.com>
2025-10-01 19:51:52 +0530

    Create iam_role_overlap.tf

---
+++ iam_role_overlap.tf
@@ -0,0 +1,33 @@
+
+
+
+resource "aws_iam_group" "app_admins" {
+  name = "app-admins"
+}
+resource "aws_iam_policy_attachment" "admin_policy" {
+  group      = aws_iam_group.app_admins.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" # Example policy
+}
+
+
+resource "aws_iam_group" "app_readonly" {
+  name = "app-readonly"
+}
+resource "aws_iam_policy_attachment" "readonly_policy" {
+  group      = aws_iam_group.app_readonly.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess" # Example policy
+}
+
+
+resource "aws_iam_user" "app_user" {
+  name = "app-deployer"
+}
+
+
+resource "aws_iam_user_group_membership" "user_membership_violation" {
+  user   = aws_iam_user.app_user.name
+  groups = [
+    aws_iam_group.app_admins.name,
+    aws_iam_group.app_readonly.name # This is redundant
+  ]
+}
