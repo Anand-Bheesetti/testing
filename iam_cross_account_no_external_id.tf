096630150bddd56c2849c053f7d49bfba0f91af0
Anand-Bheesetti <anand.bheesetti@techolution.com>
2025-10-03 14:43:12 +0530

    Update iam_cross_account_no_external_id.tf

--- iam_cross_account_no_external_id.tf
+++ iam_cross_account_no_external_id.tf
@@ -8,17 +8,17 @@ resource "aws_iam_role" "cross_account_role_violation" {
       {
         Effect = "Allow",
         Principal = {
-          # The external account that can assume this role
+          
           AWS = "arn:aws:iam::987654321098:root" 
         },
         Action = "sts:AssumeRole"
-        # VIOLATION: The mandatory "Condition" with "sts:ExternalId" is missing.
+       
       }
     ]
   })
 }
 
-# Attach a policy to the role so it can perform actions
+
 resource "aws_iam_role_policy_attachment" "s3_access_attachment" {
   role       = aws_iam_role.cross_account_role_violation.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
