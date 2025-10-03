574345a8903056293c577de19dc305932111d152
Anand-Bheesetti <anand.bheesetti@techolution.com>
2025-10-03 14:43:42 +0530

    Update iam_overprivileged.tf

--- iam_overprivileged.tf
+++ iam_overprivileged.tf
@@ -1,7 +1,7 @@
 
 resource "aws_iam_role_policy" "s3_admin_policy_violation" {
   name = "s3-admin-access-violation"
-  role = "arn:aws:iam::123456789012:role/MyApplicationRole" // Dummy role ARN for context
+  role = "arn:aws:iam::123456789012:role/MyApplicationRole" 
 
   policy = jsonencode({
     Version = "2012-10-17",
